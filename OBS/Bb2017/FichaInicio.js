9e3ec5f44d35e0351005cd9d14fb4d934ded658c
cruizb <carlosruizb@gmail.com>
2017-02-17 11:27:03 +0100

    Create FichaInicio.js
    JS de la ficha de Inicio OBS 2017, llamada desde el módulo de inicio.

---
+++ OBS/Bb2017/FichaInicio.js
@@ -0,0 +1,28 @@
+function mensajeBienvenida() {
+     
+    document.getElementById("mensajebienvenida").innerHTML = getMensaje();
+}
+
+function getMensaje(mensaje){
+	var nombre = document.getElementById("nombre").innerHTML;
+    var fecha = new Date();
+    var horasFecha = fecha.getHours();
+    var parteDia = {
+    manana: "Buenos días, ",
+    tarde: "Buenas tardes, ",	
+    noche: "Buenas noches, "  
+    };
+	console.log("Nombre Completo: " + nombre);
+    
+	if (horasFecha >= 5 && horasFecha <= 12){
+		return parteDia.manana + '<b><span style="color: #0071b3;font-family:\'Enriqueta\', sans-serif!important;font-size: 26px;">' + nombre + '!</span></b>';
+	}       
+    else if (horasFecha > 12 && horasFecha <= 21){
+    	return parteDia.tarde + '<b><span style="color: #0071b3;font-family:\'Enriqueta\', sans-serif!important;font-size: 26px;">' + nombre + '!</span></b>';
+    }
+    else{
+        return parteDia.noche + '<b><span style="color: #0071b3;font-family:\'Enriqueta\', sans-serif!important;font-size: 26px;">' + nombre + '!</span></b>';    
+	}
+}
+
+mensajeBienvenida();
