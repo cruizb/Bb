b6ffffed4baaeb069395c3fab9a3b6b5e008b64c
cruizb <carlosruizb@gmail.com>
2017-10-30 16:41:21 +0100

    Update html5videoscript.js

--- HTML5VideoScript/html5videoscript.js
+++ HTML5VideoScript/html5videoscript.js
@@ -1,7 +1,5 @@
 var videoscript = document.getElementById('html5videoscript');
 
-console.log(videoscript);
-
 var videoUrl = videoscript.getAttribute('data-videourl');
 var videoWidth = videoscript.getAttribute('data-width');
 var videoHeight = videoscript.getAttribute('data-height');
@@ -25,7 +23,7 @@ videocontainer.id = "html5scriptvideo-container";
 videocontainer.style.cssText = "display: table;position: relative;margin: auto;padding: 0px 6px;border: 2px solid rgba(1, 0, 1, 0.2);background: #fff;"
 videocontainer.innerHTML = '\
 	<div style="color: #5a5a5a;font-size: 15px;font-family: sans-serif;font-weight: bold;padding: 7px 0px;text-align: center;">' + videoTitle + '</div>\
-	<video width="' + videoWidth + '" height="' + videoHeight + '" controls poster="play.png">\
+	<video width="' + videoWidth + '" height="' + videoHeight + '" controls poster="https://github.com/cruizb/Bb/raw/master/HTML5VideoScript/play.png">\
 	<source src="' + videoUrl + '" type="video/mp4">\
 	Your browser does not support the video tag.\
 	</video>';
