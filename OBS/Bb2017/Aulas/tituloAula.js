2c404d6fb444642195fa96fffd834defe70c40ab
cruizb <carlosruizb@gmail.com>
2023-08-23 14:59:35 +0200

    Update tituloAula.js

--- OBS/Bb2017/Aulas/tituloAula.js
+++ OBS/Bb2017/Aulas/tituloAula.js
@@ -1,8 +1,8 @@
 // Se puede insertar direcamente en un módulo.
-var getTitulo = document.getElementById("courseMenu_link").innerHTML;
-var a = getTitulo.indexOf("(") + 1;
-var b = getTitulo.toString().length -1;
-var titulo = getTitulo.slice(a,b); 
-var setTitulo = document.getElementById("pageTitleText").children[0];
-setTitulo.innerHTML = titulo;
-setTitulo.style.cssText = 'font-family: "Enriqueta", sans-serif!important; color: #0071b3';
+// var getTitulo = document.getElementById("courseMenu_link").innerHTML;
+// var a = getTitulo.indexOf("(") + 1;
+// var b = getTitulo.toString().length -1;
+// var titulo = getTitulo.slice(a,b); 
+// var setTitulo = document.getElementById("pageTitleText").children[0];
+// setTitulo.innerHTML = titulo;
+// setTitulo.style.cssText = 'font-family: "Enriqueta", sans-serif!important; color: #0071b3';
